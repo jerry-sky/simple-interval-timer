e80c7aab966712032d26d601f41228275a840ce1
jerry-sky <jwroczynski@gmail.com>
2020-10-07 20:15:00 +0200

    init

---
+++ src/Program.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Program.scss";
+
+function Program() {
+    return (
+        <div className="program">
+            <div className="header">
+                <p>Interval Timer</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Program;
