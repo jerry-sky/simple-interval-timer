ae0831fd5ec48c8e3f978437069e70b6f7c8b00c
jerry-sky <jwroczynski@gmail.com>
2020-10-11 15:27:06 +0200

    fixed the EditIntervals component

--- src/EditIntervals.tsx
+++ src/EditIntervals.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, ChangeEvent, FormEvent } from "react";
+import "./EditIntervals.scss";
 
 /**
  * The object that defines one interval in the program.
@@ -24,9 +25,12 @@ interface IntervalListState {
     intervals: Interval[];
 }
 
-class EditIntervals extends Component<{}, IntervalListState> {
+export interface EditIntervalsProps {
+    submitFunction: () => any;
+}
 
-    constructor(props: {}) {
+class EditIntervals extends Component<EditIntervalsProps, IntervalListState> {
+    constructor(props: EditIntervalsProps) {
         super(props);
 
         // define some default interval values
@@ -35,32 +39,74 @@ class EditIntervals extends Component<{}, IntervalListState> {
                 {
                     id: 1,
                     interval: 25,
-                    description: 'Focus'
+                    description: "Focus",
                 },
                 {
                     id: 2,
                     interval: 15,
-                    description: 'Break'
-                }
-            ]
-        }
+                    description: "Break",
+                },
+            ],
+        };
 
         this.handleChange = this.handleChange.bind(this);
 
+        this.submitForm = (event: any) => {
+            event.preventDefault();
+            props.submitFunction();
+        };
     }
 
-    handleChange(event: Event) {
-        console.log('state changed')
+    private submitForm: (event: FormEvent<HTMLFormElement>) => any;
+
+    handleChange(event: ChangeEvent<HTMLInputElement>, id: number) {
+        const value = parseInt(event.target.value) || 0;
+        this.setState({
+            intervals: [
+                ...this.state.intervals.slice(0, id - 1),
+                {
+                    ...this.state.intervals[id - 1],
+                    interval: value,
+                },
+                ...this.state.intervals.slice(id),
+            ],
+        });
     }
 
     render() {
-        return (
-            <form>
+        /**
+         * Generates input elements for all saved intervals.
+         */
+        const generateInputElements = () => {
+            const output: JSX.Element[] = [];
+            // add the ‘autofocus’ attribute to the first element
+            let first = true;
+            for (const interval of this.state.intervals) {
+                output.push(
+                    <input
+                        autoFocus={first}
+                        className="number"
+                        key={interval.id}
+                        type="text"
+                        value={interval.interval || ""}
+                        placeholder="0"
+                        onChange={(event) =>
+                            this.handleChange(event, interval.id)
+                        }
+                    />
+                );
+                first = false;
+            }
+            return output;
+        };
 
+        return (
+            <form onSubmit={this.submitForm}>
+                <div className="intervals">{generateInputElements()}</div>
+                <button type="submit">Start</button>
             </form>
         );
     }
 }
 
 export default EditIntervals;
-
--- src/Program.tsx
+++ src/Program.tsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "./Program.scss";
+import Form from "./EditIntervals";
 
 class Program extends Component<{}> {
+
+    private submitIntervalForm() {
+        console.log('I’m going outside');
+    }
 
     render() {
         return (
             <div className="program">
                 <div className="header">
                     <p>Interval Timer</p>
+                    <Form submitFunction={this.submitIntervalForm} />
                 </div>
             </div>
         );
